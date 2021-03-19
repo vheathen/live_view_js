68e886c867b34472787c79dc2a39a0ea31ddc5ef
heathen <v.heathen@gmail.com>
2021-03-19 21:57:08 +0500

    Basic LiveView setup

--- mix.exs
+++ mix.exs
@@ -35,13 +35,15 @@ defmodule LiveViewJs.MixProject do
     [
       {:phoenix, "~> 1.5.8"},
       {:phoenix_html, "~> 2.11"},
+      {:phoenix_live_view, "~> 0.15.4"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_dashboard, "~> 0.4"},
       {:telemetry_metrics, "~> 0.4"},
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:floki, ">= 0.27.0", only: :test}
     ]
   end
 
